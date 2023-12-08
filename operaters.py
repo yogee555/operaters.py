61d4faabf19ce8fddce987f910cdedaa04de64ba
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2023-12-08 12:46:30 +0530

    Add files via upload

---
+++ operaters.py
@@ -0,0 +1,26 @@
+a = 85
+b = 10
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a//b)
+print(a%b)
+print(13%2)
+print(37%2)
+print(7%10)
+
+# Boolen type
+a = 70
+print(a)
+print(a==70)
+
+c = a==70
+print(c)
+print(type(c))
+
+c = 9
+d = 9
+print(c>d)
+print(c>=d)
+
